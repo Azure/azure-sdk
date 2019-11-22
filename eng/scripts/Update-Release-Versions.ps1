9e89c15170dcaeae67cc35bdd8a89adf738adb40
Wes Haggard <Wes.Haggard@microsoft.com>
2019-11-22 14:13:14 -0800

    Add script to update to latest package versions

---
+++ eng/scripts/Update-Release-Versions.ps1
@@ -0,0 +1,238 @@
+param (
+  $language = "all",
+  $releaseFolder = "$PSScriptRoot\..\..\_data\releases\latest"
+)
+
+function GetExistingTags($apiUrl)
+{
+  try
+  {
+    return (Invoke-RestMethod -Method "GET" -Uri "$apiUrl/git/refs/tags"  ) | % { $_.ref.Replace("refs/tags/", "") }
+  }
+  catch
+  {
+    $statusCode = $_.Exception.Response.StatusCode.value__
+    $statusDescription = $_.Exception.Response.StatusDescription
+
+    Write-Host "Failed to retrieve tags from repository."
+    Write-Host "StatusCode:" $statusCode
+    Write-Host "StatusDescription:" $statusDescription
+    exit(1)
+  }
+}
+
+function GetPackageVersions($apiUrl)
+{
+  $tags = GetExistingTags $apiUrl
+  $packageVersions = @{ }
+  $packageTags = $tags | % {
+      $sp = $_.Split('_');
+      if ($sp.Length -eq 2) {
+        $package = $sp[0];
+        $version = $sp[1];
+        if (!$packageVersions.Contains($package)) {
+          $packageVersions.Add($package, $(new-object PSObject -Property @{ Versions = @(); Latest = $version }));
+        }
+        $pv = $packageVersions[$package];
+        $pv.Versions += $version;
+
+        ## TODO: Sort based on SemVer for not take the last one
+        #if ($pv.Latest -lt $version) {
+        $pv.Latest = $version
+        #}
+      }
+    }
+  return $packageVersions
+}
+
+function CheckLink($url)
+{
+  try
+  {
+    #Write-Host "Checking $url"
+    $response = Invoke-WebRequest -Uri $url
+    return $true
+  }
+  catch
+  {
+    Write-Warning "Invalid link $url"
+  }
+  return $false
+}
+
+function Update-java-Packages($packageList, $versions)
+{
+  foreach ($pkg in $packageList)
+  {
+    $version = $versions[$pkg.Package].Latest;
+
+    if ($pkg.Version -eq $version)
+    {
+      Write-Host "Skipping $($pkg.Package) because there isn't a newer version then $version"
+      continue;
+    }
+
+    $valid = $true;
+    $valid = $valid -and (CheckLink ("https://github.com/Azure/azure-sdk-for-java/tree/{0}_{1}/sdk/{2}/{0}/" -f $pkg.Package, $version, $pkg.RepoPath))
+    $valid = $valid -and (CheckLink ("https://search.maven.org/artifact/com.azure/{0}/{1}/jar/" -f $pkg.Package, $version))
+    $valid = $valid -and (CheckLink ("https://azuresdkdocs.blob.core.windows.net/`$web/java/{0}/{1}/index.html" -f $pkg.Package, $version))
+
+    if ($valid)
+    {
+      if ($pkg.Version -ne $version)
+      {
+        Write-Host "Updating version $($pkg.Package) from $($pkg.Version) to $version"
+        $pkg.Version = $version;
+      }
+    }
+    else
+    {
+      Write-Warning "Not updating version for $($pkg.Package) because at least one associated URL is not valid!"
+    }
+
+  }
+  return $table
+}
+
+function Update-js-Packages($packageList, $versions)
+{
+  foreach ($pkg in $packageList)
+  {
+    $version = $versions["@azure/$($pkg.Package)"].Latest;
+
+    if ($pkg.Version -eq $version)
+    {
+      Write-Host "Skipping $($pkg.Package) because there isn't a newer version then $version"
+      continue;
+    }
+
+    $valid = $true;
+    $valid = $valid -and (CheckLink ("https://github.com/Azure/azure-sdk-for-js/tree/@azure/{0}_{1}/sdk/{2}/{0}/" -f $pkg.Package, $version, $pkg.RepoPath))
+    $valid = $valid -and (CheckLink ("https://www.npmjs.com/package/@azure/{0}/v/{1}" -f $pkg.Package, $version))
+    $valid = $valid -and (CheckLink ("https://azuresdkdocs.blob.core.windows.net/`$web/javascript/azure-{0}/{1}/index.html" -f $pkg.Package, $version))
+
+    if ($valid)
+    {
+      Write-Host "Updating version $($pkg.Package) from $($pkg.Version) to $version"
+      $pkg.Version = $version;
+    }
+    else
+    {
+      Write-Warning "Not updating version for $($pkg.Package) because at least one associated URL is not valid!"
+    }
+  }
+  return $table
+}
+
+function Update-net-Packages($packageList, $tf)
+{
+  foreach ($pkg in $packageList)
+  {
+    $version = $versions[$pkg.Package].Latest;
+
+    if ($pkg.Version -eq $version)
+    {
+      Write-Host "Skipping $($pkg.Package) because there isn't a newer version then $version"
+      continue;
+    }
+
+    $valid = $true;
+    $valid = $valid -and (CheckLink ("https://github.com/Azure/azure-sdk-for-net/tree/{0}_{1}/sdk/{2}/{0}/" -f $pkg.Package, $version, $pkg.RepoPath))
+    $valid = $valid -and (CheckLink ("https://www.nuget.org/packages/{0}/{1}" -f $pkg.Package, $version))
+    $valid = $valid -and (CheckLink ("https://azuresdkdocs.blob.core.windows.net/`$web/dotnet/{0}/{1}/api/index.html" -f $pkg.Package, $version))
+
+    if ($valid)
+    {
+      Write-Host "Updating version $($pkg.Package) from $($pkg.Version) to $version"
+      $pkg.Version = $version;
+    }
+    else
+    {
+      Write-Warning "Not updating version for $($pkg.Package) because at least one associated URL is not valid!"
+    }
+  }
+  return $table
+}
+
+function Update-python-Packages($packageList, $tf)
+{
+  foreach ($pkg in $packageList)
+  {
+    $version = $versions[$pkg.Package].Latest;
+
+    if ($pkg.Version -eq $version)
+    {
+      Write-Host "Skipping $($pkg.Package) because there isn't a newer version then $version"
+      continue;
+    }
+
+    # Need to have an override for an invalid package path for azure-eventhub package lives in sdk/eventhub/azure-eventhubs path. We should fix that.
+    $pkgPath = $pkg.Package
+    if ($pkg.PackagePathOverride -ne $nul) { $pkgPath = $pkg.PackagePathOverride}
+
+    $valid = $true;
+    $valid = $valid -and (CheckLink ("https://github.com/Azure/azure-sdk-for-python/tree/{0}_{1}/sdk/{2}/{3}/" -f $pkg.Package, $version, $pkg.RepoPath, $pkgPath))
+    $valid = $valid -and (CheckLink ("https://pypi.org/project/{0}/{1}" -f $pkg.Package, $version))
+    $valid = $valid -and (CheckLink ("https://azuresdkdocs.blob.core.windows.net/`$web/python/{0}/{1}/index.html" -f $pkg.Package, $version))
+
+    if ($valid)
+    {
+      Write-Host "Updating version $($pkg.Package) from $($pkg.Version) to $version"
+      $pkg.Version = $version;
+    }
+    else
+    {
+      Write-Warning "Not updating version for $($pkg.Package) because at least one associated URL is not valid!"
+    }
+  }
+  return $table
+}
+
+function Output-Latest-Versions($lang)
+{
+  $packagelistFile = Join-Path $releaseFolder "$lang-packages.csv"
+  $packageList = gc $packagelistFile | ConvertFrom-Csv | sort Service
+
+  $apiUrl = "https://api.github.com/repos/azure/azure-sdk-for-$lang"
+  Write-Host "Using API URL $apiUrl"
+
+  $versions = GetPackageVersions $apiUrl
+
+  $LangFunction = "Update-$lang-Packages"
+  &$LangFunction $packageList $versions
+
+  Write-Host "Writing $packagelistFile"
+  $packageList | ConvertTo-CSV -NoTypeInformation | out-file $packagelistFile -encoding ascii
+}
+
+switch($language)
+{
+  "all" {
+    Output-Latest-Versions "java"
+    Output-Latest-Versions "js"
+    Output-Latest-Versions "net"
+    Output-Latest-Versions "python"
+    break
+  }
+  "java" {
+    Output-Latest-Versions $language
+    break
+  }
+  "js" {
+    Output-Latest-Versions $language
+    break
+  }
+  "net" {
+    Output-Latest-Versions $language
+    break
+  }
+  "python" {
+    Output-Latest-Versions $language
+    break
+  }
+  default {
+    Write-Host "Unrecognized Language: $language"
+    exit(1)
+  }
+}
+
