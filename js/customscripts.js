52460b6661580637876ea6a95712f77a0d7acb0c
Wes Haggard <weshaggard@users.noreply.github.com>
2020-06-03 16:26:13 -0700

    Add basic filtering to the all libaries package index pages (#1378)

--- js/customscripts.js
+++ js/customscripts.js
@@ -52,3 +52,12 @@ $(function() {
         }
     });
 });
+
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
