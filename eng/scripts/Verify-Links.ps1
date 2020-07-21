d0fd066656edf2e51df7d2d2c22770bfc9e5f46d
Wes Haggard <weshaggard@users.noreply.github.com>
2020-07-21 13:03:19 -0700

    Switch to request HEAD instead of Get (#1574)
    * Switch to request HEAD instead of Get
    * Fallback to GET request if HEAD request fails

--- eng/scripts/Verify-Links.ps1
+++ eng/scripts/Verify-Links.ps1
@@ -95,7 +95,19 @@ function CheckLink ([System.Uri]$linkUri)
   }
   else {
     try {
-      $response = Invoke-WebRequest -Uri $linkUri
+      $headRequestSucceeded = $true
+      try {
+        # Attempt HEAD request first
+        $response = Invoke-WebRequest -Uri $linkUri -Method HEAD
+      }
+      catch {
+        $headRequestSucceeded = $false
+      }
+      if (!$headRequestSucceeded) {
+        # Attempt a GET request if the HEAD request failed.
+        $response = Invoke-WebRequest -Uri $linkUri -Method GET
+      }
+
       $statusCode = $response.StatusCode
       if ($statusCode -ne 200) {
         Write-Host "[$statusCode] while requesting $linkUri"
