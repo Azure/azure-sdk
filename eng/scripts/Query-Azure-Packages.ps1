fe87e698fae6fff0ae0a9c8230d58469b146fdda
Wes Haggard <Wes.Haggard@microsoft.com>
2020-07-24 20:03:17 -0700

    Add all columns to allpackages csv files

--- eng/scripts/Query-Azure-Packages.ps1
+++ eng/scripts/Query-Azure-Packages.ps1
@@ -3,17 +3,55 @@ param (
   $folder =  "$PSScriptRoot\..\..\_data\allpackages"
 )
 
-function Query-java-Packages
+function PackageEqual($pkg1, $pkg2) {
+  if ($pkg1.Package -ne $pkg2.Package) {
+    return $false
+  }
+  if ($pkg1.GroupId -and $pkg2.GroupId -and $pkg1.GroupId -ne $pkg2.GroupId) {
+    return $false
+  }
+  return $true
+}
+function CreatePackage(
+  [string]$package, 
+  [string]$version,
+  [string]$groupId = ""
+)
+{
+  $versionGA = $versionPreview = ""
+  if ($version -match "^[\d\.]+$") {
+    $versionGA = $version
+  }
+  else {
+    $versionPreview = $version
+  }
+
+  return [PSCustomObject][ordered]@{
+    DisplayName = $package
+    ServiceName = ""
+    Package = $package
+    GroupId = $groupId
+    VersionGA = $versionGA
+    VersionPreview = $versionPreview
+    RepoPath = ""
+    MSDocs = ""
+    GHDocs = ""
+    Type = ""
+    Hide = ""
+    Notes = ""
+  };
+}
+function Get-java-Packages
 {
   # Rest API docs https://search.maven.org/classic/#api
   $mavenQuery = Invoke-RestMethod "https://search.maven.org/solrsearch/select?q=g:com.microsoft.azure*%20OR%20g:com.azure*&rows=1000&wt=json"
 
   Write-Host "Found $($mavenQuery.response.numFound) maven packages"
-  $packages = $mavenQuery.response.docs | Foreach-Object { [pscustomobject]@{ DisplayName = $_.a; Package = $_.a; GroupId = $_.g; Version = $_.latestVersion; } }
+  $packages = $mavenQuery.response.docs | Foreach-Object { CreatePackage $_.a $_.latestVersion $_.g }
   return $packages
 }
 
-function Query-dotnet-Packages
+function Get-dotnet-Packages
 {
   # Rest API docs
   # https://docs.microsoft.com/en-us/nuget/api/search-query-service-resource
@@ -21,11 +59,11 @@ function Query-dotnet-Packages
   $nugetQuery = Invoke-RestMethod "https://azuresearch-usnc.nuget.org/query?q=owner:azure-sdk&prerelease=true&semVerLevel=2.0.0&take=1000"
 
   Write-Host "Found $($nugetQuery.totalHits) nuget packages"
-  $packages = $nugetQuery.data | Foreach-Object { [pscustomobject]@{ DisplayName = $_.id; Package = $_.id; Version = $_.version } }
+  $packages = $nugetQuery.data | Foreach-Object { CreatePackage $_.id $_.version }
   return $packages
 }
 
-function Query-js-Packages
+function Get-js-Packages
 {
   $from = 0
   $npmPackages = @()
@@ -43,11 +81,11 @@ function Query-js-Packages
   } while ($npmQuery.objects.Count -ne 0);
 
   Write-Host "Found $($npmPackages.Count) npm packages"
-  $packages = $npmPackages | Foreach-Object { [pscustomobject]@{ DisplayName = $_.name; Package = $_.name; Version = $_.version } }
+  $packages = $npmPackages | Foreach-Object { CreatePackage $_.name $_.version }
   return $packages
 }
 
-function Query-python-Packages
+function Get-python-Packages
 {
   $pythonQuery = "import xmlrpc.client; [print(pkg[1]) for pkg in xmlrpc.client.ServerProxy('https://pypi.org/pypi').user_packages('azure-sdk')]"
   $pythonPackagesNames = (python -c "$pythonQuery")
@@ -55,40 +93,45 @@ function Query-python-Packages
   $pythonPackages = $pythonPackagesNames | Foreach-Object { try { (Invoke-RestMethod "https://pypi.org/pypi/$_/json").info } catch { } }
 
   Write-Host "Found $($pythonPackages.Count) python packages"
-  $packages = $pythonPackages | Foreach-Object { [pscustomobject]@{ DisplayName = $_.name; Package = $_.name; Version = $_.version } }
+  $packages = $pythonPackages | Foreach-Object { CreatePackage $_.name $_.version }
   return $packages
 }
 
-function Output-Latest-Versions($lang)
+function Write-Latest-Versions($lang)
 {
   $packagelistFile = Join-Path $folder "$lang-packages.csv"
   $packageList = Import-Csv $packagelistFile
 
   if ($null -eq $packageList) { $packageList = @() }
 
-  $extraProperties = [ordered]@{
-    Hide = ""
-    Notes = ""
-    Type = ""
-  }
-  $LangFunction = "Query-$lang-Packages"
-  $packages= &$LangFunction
+  $LangFunction = "Get-$lang-Packages"
+  $packages = &$LangFunction
 
   foreach ($pkg in $packages)
   {
-    $pkgEntries = $packageList | Where-Object { $_.Package -eq $pkg.Package -and $_.GroupId -eq $pkg.GroupId }
+    $pkgEntries = $packageList | Where-Object { PackageEqual $_ $pkg }
 
     if ($pkgEntries.Count -gt 1) {
-      Write-Error "Found $($pkgEntry.Count) package entries for $pkg.Package"
+      Write-Error "Found $($pkgEntries.Count) package entries for $($pkg.Package + $pkg.GroupId)"
     }
     elseif ($pkgEntries.Count -eq 0) {
       # Add package
-      $pkg | Add-Member -NotePropertyMembers $extraProperties -Force
       $packageList += $pkg
     }
     else {
       # Update version of package
-      $pkgEntries[0].Version = $pkg.Version
+      $pkgEntries[0].VersionGA = $pkg.VersionGA
+      $pkgEntries[0].VersionPreview = $pkg.VersionPreview
+    }
+  }
+
+  # Clean out packages that are no longer in the query we use for the package manager
+  foreach ($pkg in $packageList)
+  {
+    $pkgEntries = $packages | Where-Object { PackageEqual $_ $pkg }
+
+    if ($pkgEntries -and $pkgEntries.Count -ne 1) {
+      Write-Host "Found package $($pkg.Package) in the CSV which should be removed"
     }
   }
 
@@ -100,26 +143,26 @@ function Output-Latest-Versions($lang)
 switch($language)
 {
   "all" {
-    Output-Latest-Versions "java"
-    Output-Latest-Versions "js"
-    Output-Latest-Versions "dotnet"
-    Output-Latest-Versions "python"
+    Write-Latest-Versions "java"
+    Write-Latest-Versions "js"
+    Write-Latest-Versions "dotnet"
+    Write-Latest-Versions "python"
     break
   }
   "java" {
-    Output-Latest-Versions $language
+    Write-Latest-Versions $language
     break
   }
   "js" {
-    Output-Latest-Versions $language
+    Write-Latest-Versions $language
     break
   }
   "dotnet" {
-    Output-Latest-Versions $language
+    Write-Latest-Versions $language
     break
   }
   "python" {
-    Output-Latest-Versions $language
+    Write-Latest-Versions $language
     break
   }
   default {
