4652aa504280090dc2298fa1ea370c0d2526477e
Wes Haggard <Wes.Haggard@microsoft.com>
2020-07-24 21:22:14 -0700

    Add Type and use Update-Release-Versions.ps1 script

--- eng/scripts/Update-Release-Versions.ps1
+++ eng/scripts/Update-Release-Versions.ps1
@@ -1,6 +1,6 @@
 param (
   $language = "all",
-  $releaseFolder = "$PSScriptRoot\..\..\_data\releases\latest"
+  $releaseFolder = "$PSScriptRoot\..\..\_data\allpackages"#"$PSScriptRoot\..\..\_data\releases\latest"
 )
 
 $releaseFolder = Resolve-Path $releaseFolder
@@ -247,10 +247,13 @@ function Update-python-Packages($packageList)
 function Output-Latest-Versions($lang)
 {
   $packagelistFile = Join-Path $releaseFolder "$lang-packages.csv"
-  $packageList = Get-Content $packagelistFile | ConvertFrom-Csv | Sort-Object DisplayName
+  $packageList = Get-Content $packagelistFile | ConvertFrom-Csv | Sort-Object Package, GroupId, DisplayName
+
+  # Only update libraries that have a type
+  $clientPackages = $packageList | Where-Object { $_.Type }
 
   $LangFunction = "Update-$lang-Packages"
-  &$LangFunction $packageList 
+  &$LangFunction $clientPackages 
 
   Write-Host "Writing $packagelistFile"
   $packageList | ConvertTo-CSV -NoTypeInformation -UseQuotes Always | Out-File $packagelistFile -encoding ascii
