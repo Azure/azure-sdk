fccc905214c0b458bec0ae1f3836a9a821be267b
Jon Gallant <jon@jongallant.com>
2020-08-03 13:59:22 -0700

    only search first column

--- js/customscripts.js
+++ js/customscripts.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
     $("#myInput").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $("#myTable tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        $(this).toggle($(this).find("td:first").text().toLowerCase().indexOf(value) > -1)
       });
     });
   });
