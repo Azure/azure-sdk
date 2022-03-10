1099da4ca9328da66a50368457e426f0fb23dcc3
Wes Haggard <Wes.Haggard@microsoft.com>
2022-03-09 16:00:29 -0800

    Update js parsing logic to handle profile packages in js

--- eng/scripts/Query-Azure-Packages.ps1
+++ eng/scripts/Query-Azure-Packages.ps1
@@ -72,7 +72,7 @@ function Get-js-Packages
   {
     # If package starts with arm- and we shipped it recently because it is in the last months repo tags
     # then treat it as a new mgmt library
-    if ($package.Package -match "@azure/arm-(?<serviceName>.*)" -and $repoTags.ContainsKey($package.Package))
+    if ($package.Package -match "^@azure/arm-(?<serviceName>.*?)(-profile.*)?$" -and $repoTags.ContainsKey($package.Package))
     {
       $serviceName = (Get-Culture).TextInfo.ToTitleCase($matches["serviceName"])
       $package.Type = "mgmt"
