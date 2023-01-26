b428e207eb09c592ad715e3ebaa06d12802415c5
Ronnie Geraghty <rgeraghty@microsoft.com>
2023-01-25 16:05:58 -0800

    Adding inventory dashboard (#5417)
    * Moving Inventory Dashboard scripts to repo
    
    * adding myself as codeowner for inventory dashboard scripts
    
    * fixes #5408, release CSVs pulled in from local disk
    
    * removed un-used dependencies
    
    * fixes #5409 pull rest api specs from local storage
    
    * Added clone step for azure-rest-api-specs repo
    
    * changed path for azure-rest-api-specs repo
    
    * fixed yaml formatting change
    
    * changed location of produced inventory.csv file
    
    * removed PowerBI binary
    
    * adding initial version of inventory.csv
    
    * Fixed bug of missing package saying it was created from a file path instead of API Spec Dir

---
+++ eng/scripts/inventory-dashboard/src/csvData.ts
@@ -0,0 +1,61 @@
+import csvToJSON from "csvtojson";
+import { Language } from "./types";
+import path from "path";
+import fs from 'fs';
+import { Logger } from './logger';
+const log = Logger.getInstance();
+const csvDirPath = path.join(__dirname, `../../../../_data/releases/latest`);
+
+/**
+ * Reads CSVs files. 
+ * Converts CSV File to JSON Array and adds Language Property for each package. 
+ * @returns Promise<any[]> all pkg objects from all languages in one array 
+ */
+export default async function collectCSVData(): Promise<any[]> {
+  // Retrieve CSV files from latest release dir
+  const csvFiles = fs.readdirSync(csvDirPath);
+  // Get CSV contents from all files and convert to JSON
+  const packagesJSONArr: any[] = []; // Array to contain all CSV contents
+  for (let csv of csvFiles) {
+    // Convert csv file to json arr
+    const jsonContent = await csvToJSON().fromFile(path.join(csvDirPath, csv));
+    // Add Language property to each pkg object 
+    for (let pkg of jsonContent) {
+      packagesJSONArr.push({
+        ...pkg,
+        Language: csvNameToLanguage(csv)
+      });
+    }
+  }
+  return packagesJSONArr;
+}
+
+/**
+ * Takes the CSV File name from a CSV in the Azure/azure-sdk/_data/releases/latest repo dir and returns the corresponding Language type
+ * @param csvName Name of CSV File from Azure/azure-sdk/_data/releases/latest repo dir
+ * @returns Language type based on CSV name
+ */
+function csvNameToLanguage(csvName: string): Language {
+  switch (csvName) {
+    case "android-packages.csv":
+      return "Android";
+    case "c-packages.csv":
+      return "C";
+    case "cpp-packages.csv":
+      return "C++";
+    case "dotnet-packages.csv":
+      return ".NET";
+    case "go-packages.csv":
+      return "Go";
+    case "ios-packages.csv":
+      return "iOS";
+    case "java-packages.csv":
+      return "Java";
+    case "js-packages.csv":
+      return "JavaScript";
+    case "python-packages.csv":
+      return "Python";
+    default:
+      throw Error(`Package CSV matches no Language. Package name: ${csvName}`);
+  }
+}
