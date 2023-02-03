ba129f8aa57cdcab8dc756c5c12accfad058ce94
Ronnie Geraghty <rgeraghty@microsoft.com>
2023-02-03 14:10:36 -0800

    Fixes Exception Revamp #5492

--- eng/scripts/inventory-dashboard/src/exceptionHandler.ts
+++ eng/scripts/inventory-dashboard/src/exceptionHandler.ts
@@ -1,44 +1,77 @@
-import { Tier1Languages, PackageList, TrackSpecificsDefault } from "./types";
-import _exceptionsMap from '../data-and-rules/exceptionsMap.json';
+import { isLanguage, isPlaneType, Language, Package, PackageList, TrackSpecificsDefault } from "./types";
+import _exceptionsList from '../data-and-rules/exceptionsList.json';
 import { Logger } from './logger';
 const log = Logger.getInstance();
-const exceptionMap: any = _exceptionsMap;
+const exceptionList: any = _exceptionsList;
+/**
+ * Apply exceptions from the exceptionsMap.json file to the list of packages.
+ * @param packages Map of found packages.
+ * @returns Map of packages with added exceptions
+ */
 export default function exceptionHandler(packages: PackageList): PackageList {
-    /*  Handel No Language SDK Needed
-        Description: When a library is needed for one programming language but not another this exception will 
-        over write the empty/missing SDK entry with an Exception. 
-    */
-    if (exceptionMap["language-sdk-not-needed"] && Array.isArray(exceptionMap["language-sdk-not-needed"])) {
-        for (let exception of exceptionMap["language-sdk-not-needed"]) {
-            if (typeof exception === 'object' && exception.Service && exception.SDK && exception.Plane && exception['Languages-Not-Needed'] && Array.isArray(exception['Languages-Not-Needed'])) {
-                for (let language of exception['Languages-Not-Needed']) {
-                    let key = (exception.Service + exception.SDK + exception.Plane + language).toLowerCase();
-                    log.info(`Handling Exception "Language SDK Not Needed" for language: ${language} in exception: ${JSON.stringify(exception)}`);
-                    if (packages[key]) {
-                        packages[key] = { ...packages[key], Track2: { ...TrackSpecificsDefault, Package: "Exception: Not Needed for Language" } };
-                    } else {
-                        log.atn(`Needless Exception Rule: "language-sdk-not-needed": Language: ${language} - Exception: ${JSON.stringify(exception)}\nConsider removing the unused rule or modifying it.`);
+    if (!Array.isArray(exceptionList)) {
+        log.err(`ExceptionList.json does not contain an array at the highest level. `);
+        return packages;
+    }
+    // Loop through all exception and apply them 
+    for (let exception of exceptionList) {
+        // check exception is an object
+        if (typeof exception !== 'object') {
+            log.err(`Exception is not an object. Exception: ${JSON.stringify(exception)}`);
+        }
+        // check exception object has Service, Exception, and SDKs properties and that they are the right type
+        else if (!(exception.hasOwnProperty('Service') && typeof exception.Service === "string") || !(exception.hasOwnProperty('Exception') && typeof exception.Exception === 'string') || !(exception.hasOwnProperty('SDKs') && typeof exception.SDKs === 'object')) {
+            log.err(`Exception missing Service:string and/or Exception:string and/or SDKs:object property. Exception: ${JSON.stringify(exception)}`);
+        }
+        // All above checks have passed
+        else {
+            // extract props to constants
+            const { Exception: exceptionStr, Service: service, SDKs: sdks } = exception;
+            // loop through SDKs object. 
+            for (let sdkName of Object.keys(sdks)) {
+                const sdk = sdks[sdkName];
+                // prop checks
+                if (!(typeof sdk === 'object' && sdk.hasOwnProperty('Plane') && typeof sdk.Plane === 'string' && isPlaneType(sdk.Plane) && sdk.hasOwnProperty('Languages') && Array.isArray(sdk.Languages))) {
+                    log.err(`SDK is not an object and/or SDK does not have props Plane:string:PlaneType and/or Languages:array.\n\tException: ${JSON.stringify(exception)}\n\tSDK Name: ${sdkName}\n\tSDK: ${JSON.stringify(sdk)}`);
+                }
+                // All above checks pass
+                else {
+                    // extract props to constants
+                    const { Plane: plane, Languages: languages } = sdk;
+                    // loop through languages
+                    for (let language of languages) {
+                        // check if language is in list of languages 
+                        if (!isLanguage(language)) {
+                            log.err(`Language is not a accepted programming language. \n\tException: ${JSON.stringify(exception)}\n\tSDK: ${JSON.stringify(sdk)}\n\tLanguage: ${language}`);
+                        }
+                        // all above checks pass
+                        else {
+                            // create package map key
+                            const key = (service + sdkName + plane + language).toLowerCase();
+                            // check if there is a matching package entry for the key
+                            if (packages[key] === undefined) {
+                                printExceptionWarn("Found Unused Exception", exception, sdkName, language);
+                            }
+                            // check if exception would write over a track 2 package
+                            else if (packages[key].Track2.ColorCode == 10) {
+                                printExceptionWarn("Exception would overwrite valid Track 2 library", exception, sdkName, language, packages[key]);
+                            }
+                            // all above checks pass
+                            else {
+                                // write over missing package entry with exception entry
+                                packages[key] = {
+                                    ...packages[key], Track2: { ...TrackSpecificsDefault, Package: `Exception: ${exceptionStr}` }
+                                };
+                            }
+                        }
                     }
                 }
             }
         }
     }
-    /*  Track 2 Replacement Not Needed
-        Description: When a Track 1 library exists with no Track 2 replacement and we do not intend to ever 
-        create a replacing library. This exception will over write the empty/missing SDK entry with an Exception. 
-     */
-    if (exceptionMap["track-2-not-needed"] && Array.isArray(exceptionMap["track-2-not-needed"])) {
-        for (let exception of exceptionMap["track-2-not-needed"]) {
-            if (typeof exception === 'object' && exception.Service && exception.SDK && exception.Plane && exception.Language) {
-                let key = (exception.Service + exception.SDK + exception.Plane + exception.Language).toLowerCase();
-                log.info(`Handling Exception "Track 2 Not Needed" for exception: ${JSON.stringify(exception)}`);
-                if (packages[key]) {
-                    packages[key] = { ...packages[key], Track2: { ...TrackSpecificsDefault, Package: "Exception: Track 2 Replacement Not Needed" } };
-                } else {
-                    log.atn(`Needless Exception Rule: "track-2-not-needed":${JSON.stringify(exception)}\nConsider removing the unused rule or modifying it.`);
-                }
-            }
-        }
-    }
     return packages;
+}
+
+function printExceptionWarn(message: string, exception: object, sdkName: string, language: Language, pkg?: Package): void {
+    log.warn(`${message}\n\tException: ${JSON.stringify(exception)}\n\tSDK: ${JSON.stringify(sdkName)}\n\tLanguage: ${language}${pkg !== undefined ? `\n\tPackage: ${JSON.stringify(pkg)}` : ''}`);
 }
